4a3ea9701408ebf9cc597f4555db66fc97cff51d
Carlos <carlos.monclef@github.com>
2020-09-17 11:54:39 -0500

    Face detection

--- Smile_Detector.py
+++ Smile_Detector.py
@@ -17,8 +17,21 @@ while True:
     if not successful_frame_read:
         break
 
+    # Change to grayscale (look at Haar Cascade of why we use this)
+    frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces [return a list of points]
+    faces = face_detector.detectMultiScale(frame_grayscale)
+    
+    # Run smile detection in those faces
+    for(x, y, w, h) in faces:
+        # Draw a rectangle around the face in the COLOR FRAME. (the numbers
+        # are the color code and the 4 means the border thickness)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 200, 50), 4)
+
     # Show the current frame from the webcam
     cv2.imshow('Smile Detector', frame)
+
 
     # Display every 1 milisecond if not key pressed 
     cv2.waitKey(1)
